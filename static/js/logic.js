2341dce6fd137d45ea9fc712979c815101e206be
shrapple <shrallen@icloud.com>
2024-01-29 15:41:46 -0600

    markers finally!!!!!

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,42 +1,37 @@
-function createMap(earthquakes){
-
-    let tilelayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {attribution: '&copy; <a href= "https://www.openstreetmap.org/copyright">'}
-    );
-
-const baseMap = {
-    'base map': tilelayer
-};
-
-const earthquakeOverlay = {
-    'earthquakes': L.layerGroup(earthquakes)
-};
-
-const myMap = L.map('map', {
-    center: [37.7566, -119.5969],
-    layers: [tilelayer,L.layerGroup(earthquakes)],
-    zoom: 7
+let tilelayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
-L.control.layers(baseMap, earthquakeOverlay).addTo(myMap);
-return myMap;
+let baseMap = {
+  'Base Map': tilelayer
 };
+
+let earthquakeLayer = {
+  'Earthquakes': L.layerGroup()
+};
+
+let myMap = L.map('map', {
+  center: [37.7566, -119.5969],
+  layers: [baseMap['Base Map'], earthquakeLayer['Earthquakes']],
+  zoom: 7
+});
+
+L.control.layers(baseMap, earthquakeLayer).addTo(myMap);
 
 const link = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson';
 
 function createMarkers(response) {
-    console.log(response);
-    eFeatures = response.features;
-    earthquakeArray = [];
+  let eFeatures = response.features;
+  for (let i = 0; i < eFeatures.length; i++) {
+    let eFeature = eFeatures[i];
+    let eCoords = eFeature.geometry.coordinates;
+    let eMag = eFeature.properties.mag;
+    let eMarker = L.marker([eCoords[1], eCoords[0]]);
+    eMarker.bindPopup(`Magnitude: ${eMag}`);
+    earthquakeLayer['Earthquakes'].addLayer(eMarker);
+  }
+}
 
-    for (let i = 0; i < eFeatures.length; i ++) {
-        let eMarker = L.marker(eFeatures[i].coordinates).bindPopup(eFeatures[i].mag);
-        earthquakeArray.push(eMarker);
-    };
-
-    let eLayer = L.layerGroup(earthquakeArray);
-    createMap(eLayer)
-
-    }
-
-d3.json(link).then(createMarkers);
+fetch(link)
+  .then(response => response.json())
+  .then(createMarkers);
