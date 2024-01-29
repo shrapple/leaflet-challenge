fe5f311bbd3bfc23c57b5f49e1c51b40eca84d6c
shrapple <shrallen@icloud.com>
2024-01-29 16:14:24 -0600

    getting closer...

--- static/js/logic.js
+++ static/js/logic.js
@@ -21,16 +21,40 @@ L.control.layers(baseMap, earthquakeLayer).addTo(myMap);
 const link = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson';
 
 function createMarkers(response) {
-  let eFeatures = response.features;
-  for (let i = 0; i < eFeatures.length; i++) {
-    let eFeature = eFeatures[i];
-    let eCoords = eFeature.geometry.coordinates;
-    let eMag = eFeature.properties.mag;
-    let eMarker = L.marker([eCoords[1], eCoords[0]]);
-    eMarker.bindPopup(`Magnitude: ${eMag}`);
-    earthquakeLayer['Earthquakes'].addLayer(eMarker);
+    let eFeatures = response.features;
+    console.log(response);
+    for (let i = 0; i < eFeatures.length; i++) {
+      let eFeature = eFeatures[i];
+      let eCoords = eFeature.geometry.coordinates;
+      let eMag = eFeature.properties.mag;
+      let eDepth = eCoords[2];
+      let eRadius = eMag * 2;
+      let depthColor = getColor(eDepth);
+      function getColor(depth) {
+        if (depth <= 10) {
+          return "#00FF00"; // Green
+        } else if (depth <= 30) {
+          return "#FFFF00"; // Yellow
+        } else if (depth <= 50) {
+          return "#FFA500"; // Orange
+        } else {
+          return "#FF0000"; // Red
+        }
+      }
+      let eMarker = L.circleMarker([eCoords[1], eCoords[0]], {
+        radius: eRadius,
+        fillColor: depthColor,
+        fillOpacity: 0.7,
+        color: "#000",
+        weight: 1, 
+        className: 'earthquake-marker'
+      });
+      
+      eMarker.bindPopup(`Magnitude: ${eMag}<br>Depth: ${eDepth}`);
+      
+      earthquakeLayer['Earthquakes'].addLayer(eMarker);
+    }
   }
-}
 
 fetch(link)
   .then(response => response.json())
